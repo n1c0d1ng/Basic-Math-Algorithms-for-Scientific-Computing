e9ccf8b942eee918bab855816975f0f2c3eb12cd
Nicolas Schäfer <31781799+n1c0d1ng@users.noreply.github.com>
2025-09-08 12:17:47 +0200

    Neue Ordnerstruktur aufgebaut

---
+++ Paramter_Estimation_Numeric_ODE/LS_Sensitivity_Approach.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Definition des dynamischen Systenms
+
+
+# Definition Sensitivity Equations
+
+
+# Loss Function 
+
+# Gradient der Loss Function 
+
+# Gradientenverfahren
